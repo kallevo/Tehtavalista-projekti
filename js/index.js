4521a7baf7ea2b466cfcfb31c5d7e07d7fe1b524
Kalle <kalle.voutilainen@metropolia.fi>
2021-04-21 12:39:19 +0300

    Lisätty muistiinpanon poistotoiminto.

--- js/index.js
+++ js/index.js
@@ -4,19 +4,35 @@ window.onload = function () {
     let nappi = document.getElementById('new1');
 
     nappi.addEventListener('click', function () {
-        let divi = document.createElement('div');
-        let divi2 = document.createElement('div');
-        document.getElementById('first').append(divi, divi2);
-        divi.setAttribute('class', 'notehead');
-        divi2.setAttribute('class', 'note-text');
-        divi2.setAttribute('id', 'teksti1');
-        let h2 = document.createElement('h2');
-        h2.setAttribute('id', 'otsikko1');
-        divi.appendChild(h2);
-        h2.innerHTML = otsikkokentta.value;
-        divi2.innerHTML = tekstikentta.value;
-        otsikkokentta.value = "";
-        tekstikentta.value = "";
+        //Katsotaan onko joku kenttä tyhjä.
+        if (otsikkokentta.value === ""|| tekstikentta.value === "") {
+            return false;
+        } else {
+            //Elementtien lisäys dom-puuhun.
+            let divi = document.createElement('div');
+            let divi2 = document.createElement('div');
+            document.getElementById('first').append(divi, divi2);
+            divi.setAttribute('class', 'notehead');
+            divi2.setAttribute('class', 'note-text');
+            divi2.setAttribute('id', 'teksti1');
+            let h2 = document.createElement('h2');
+            h2.setAttribute('id', 'otsikko1');
+            divi.appendChild(h2);
+            h2.innerHTML = otsikkokentta.value;
+            divi2.innerHTML = tekstikentta.value;
+            //Muistiinpanon poisto.
+            let poista = document.createElement('button');
+            poista.setAttribute('class', 'deletebutton');
+            poista.innerText += 'Poista';
+            divi2.appendChild(poista);
+            poista.addEventListener('click', function () {
+                divi.remove();
+                divi2.remove();
+            })
+            //Laitetaan lopuksi kentät tyhjiksi.
+            otsikkokentta.value = "";
+            tekstikentta.value = "";
+        }
     })
 }
 
