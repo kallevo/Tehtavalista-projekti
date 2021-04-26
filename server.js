9d1368c4700bfbbde745d5385b940383155f4a3f
Kalle <kalle.voutilainen@metropolia.fi>
2021-04-26 21:30:27 +0300

    Ongelma tietokantaan siirtämisessä.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ let mysql = require('mysql');
 let express = require('express');
 let app = express();
 let path = require('path');
+let bodyParser = require('body-parser');
 
 const conn = mysql.createConnection({
     host: "mysql.metropolia.fi",
@@ -15,15 +16,40 @@ conn.connect(function(err) {
     console.log("Connected to MySQL!");
 });
 
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // for reading JSON
+let util = require('util'); // for async calls
+const query = util.promisify(conn.query).bind(conn);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/index", function (req, res) {
     res.sendFile(__dirname + "/" + "index.html");
 })
 
-app.post("/post", function (req, res) {
+app.post("/post", urlencodedParser, function (req, res) {
+    let sql;
+    console.log("body: %j", req.body);
+    let jsonObj = req.body;
+    console.log("Otsikko: " + jsonObj.otsikko);
 
+    sql = "INSERT INTO taulukko1(id, rivi, otsikko, teksti)"
+        + " VALUES ( ?, ?, ?, ?)";
+
+    let responseString = JSON.stringify(jsonObj)
+    res.send("POST succesful: "+ responseString);
+
+    (async () => { // IIFE (Immediately Invoked Function Expression)
+        try {
+            const result = await query(sql, [jsonObj.id, jsonObj.rivi, jsonObj.otsikko, jsonObj.teksti]);
+        }
+        catch (err) {
+            console.log("Database error!"+ err);
+        }
+    })()
 });
+
+
 
 let server = app.listen(8080, function () {
     let host = server.address().address
